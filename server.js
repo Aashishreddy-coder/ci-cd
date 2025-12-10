801dbb33a56d4d9024cf8e2a48783e24aef04700
AASHISH REDDY <aashishreddythadur@gmail.com>
2025-12-10 15:12:28 +0530

    Intial

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+require('dotenv').config();
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT=process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+app.get('/health',(req,res)=>{
+    res.send("OK");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
